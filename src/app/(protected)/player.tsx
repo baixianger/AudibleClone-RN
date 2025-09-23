2bec307a63a0d5141a364c8bc334455a9e20a564
baixianger <58243749+baixianger@users.noreply.github.com>
2025-09-23 17:04:49 +0200

    player screen UI

--- src/app/(protected)/player.tsx
+++ src/app/(protected)/player.tsx
@@ -1,17 +1,41 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import { router } from "expo-router";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo, Ionicons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
+import PlaybackBar from "@/components/PlaybackBar";
+
+import books from "@/dummyBooks";
 
 export default function Player() {
+  const book = books[0];
   return (
-    <View className="flex-1 justify-center items-center">
+    <SafeAreaView className="flex-1 py-10 gap-y-4">
       <Pressable
         onPress={() => router.back()}
         className="absolute top-16 left-4 bg-gray-800 rounded-full p-2"
       >
         <Entypo name="chevron-down" size={24} color="white" />
       </Pressable>
-      <Text className="text-white text-2xl">Player</Text>
-    </View>
+
+      <Image
+        source={{ uri: book.thumbnail_url }}
+        className="w-[95%] aspect-square rounded-[20px] self-center"
+      />
+
+      <View className="flex-1 gap-8 justify-end">
+        <Text className="text-white text-2xl font-bold text-center">
+          {book.title}
+        </Text>
+        <PlaybackBar value={0.1} />
+
+        <View className="flex-row items-center justify-between mt-8">
+          <Ionicons name="play-skip-back" size={24} color="white" />
+          <Ionicons name="play-back" size={24} color="white" />
+          <Ionicons name="play" size={50} color="white" />
+          <Ionicons name="play-forward" size={24} color="white" />
+          <Ionicons name="play-skip-forward" size={24} color="white" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
